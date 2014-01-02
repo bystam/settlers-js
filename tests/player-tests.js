3f1d75a0477e79ccf0103a2df23af5983f1cc23e
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-01-03 00:32:56 +0100

    adds test for names of new players

--- tests/player-tests.js
+++ tests/player-tests.js
@@ -4,8 +4,17 @@ exports.testBasicPlayerCreation = function(test) {
 	test.expect(2);
 	var p;
 	test.doesNotThrow ( function () {
-		p = new players.Player("Fredrik")
+		p = new players.Player(null)
 	})
 	test.ok(p)
+	test.done()
+}
+
+exports.testNameAssigningWorks = function(test) {
+	test.expect(2)
+	var p1 = new players.Player("Fredrik")
+	var p2 = new players.Player("Lolfi")
+	test.equal(p1.name, "Fredrik")
+	test.equal(p2.name, "Lolfi")
 	test.done()
 }
