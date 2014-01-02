3baa9ad8af5da705a4114c8bd24c55c708709cb4
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-01-03 00:27:35 +0100

    adds failing test for player creation

--- tests/player-tests.js
+++ tests/player-tests.js
@@ -1,1 +1,10 @@
-var players = require('../model/players.js');
+var players = require('../model/players.js')
+
+exports.testBasicPlayerCreation = function(test) {
+	test.expect(2);
+	test.doesNotThrow ( function () {
+		p = new players.Player("Fredrik")
+	})
+	test.ok(player)
+	test.done()
+}
