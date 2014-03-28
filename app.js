740a3b9cc9a40219996edc80c4c8c5fbbfe51ace
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-03-28 21:20:30 +0100

    adds a scale app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express')
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+// express app configuring
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.static(__dirname + '/public'))
+
+// setup root route
+app.get('/', function(req, res) {
+	res.end('Hello world');
+});
+
+// Heroku specific configuration
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+// setup socket.io echoing
+io.sockets.on('connection', function (socket) {
+	socket.on('to-server', function(data) {
+		io.sockets.emit('to-client', data );
+	});
+});
+
+// start server listening
+var port = process.env.PORT || 5000;
+server.listen(port, function() {
+	console.log("Listening on port: " + port);
+});
