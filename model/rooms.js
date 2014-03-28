73f75aa01bcfdc75e627bc8ad51f04d3daa8f307
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-03-28 21:53:20 +0100

    can now emit room entered events

--- model/rooms.js
+++ model/rooms.js
@@ -5,15 +5,7 @@ var games = {};
 exports.init = function(server) {
 	io.listen(server);
 
-	io.sockets.on('connection', function (socket) {
-		socket.on('room', function (client) {
-			var room = client.room;
-			var playerId = client.playerId;
-			if (games[room] === null)
-				registerRoom(room);
-			registerPlayer(room, playerId, socket);
-		});
-	});
+	io.sockets.on('connection', newConnection);
 }
 
 // Heroku specific configuration
@@ -22,10 +14,22 @@ io.configure(function () {
   io.set("polling duration", 10); 
 });
 
+function newConnection = function (socket) {
+	socket.on('room', function (client) {
+		var room = client.room;
+		var playerId = client.playerId;
+		if (games[room] === null)
+			registerRoom(room);
+		registerPlayer(room, playerId, socket);
+	});
+}
+
 function registerRoom(room) {
 	games[room] = {};
 }
 
 function registerPlayer (socket, room, playerId) {
 	socket.join(room);
+	socket.emit('room-joined', {room: room});
+	io.sockets.in(room).emit('new-player-joined', { playerId: playerId });
 }
