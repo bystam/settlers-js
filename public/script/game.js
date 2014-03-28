e6cce616c4bd2d617e725f16e57c0834e3e4361c
Jens Arvidsson <jens.arvidsson@videoplaza.com>
2014-03-28 22:46:11 +0100

    adds button css

--- public/script/game.js
+++ public/script/game.js
@@ -5,6 +5,9 @@ $ (document).ready(function(){
 	socket.on("room-joined", function(data){
 		$("body").append("Successfully joined room: "+data.room);
 	});
+	socket.on("room-404", function(data){
+		alert("THIS FUCKING ROOM DOESNT EXIST!!!")
+	})
 	socket.on("new-player-joined", function(data){
 		$("body").append("<p>Player joined: "+data.playerId+"</p>");
 	});
