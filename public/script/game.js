918ca879143315e1bf362ab60165e60ede15ab48
Jens Arvidsson <jens.arvidsson@videoplaza.com>
2014-03-28 22:22:32 +0100

    bugfix game.js

--- public/script/game.js
+++ public/script/game.js
@@ -1,10 +1,11 @@
 $ (document).ready(function(){
 	var roomUrl = window.location.href.split("/").pop();
-	io.emit("room", {playerId:Math.random(), room:roomUrl})
-	io.on("room-joined", function(data){
+	var socket = io.connect('http://localhost:5000');
+	socket.emit("room", {playerId:Math.random(), room:roomUrl})
+	socket.on("room-joined", function(data){
 		$("body").append("Successfully joined room: "+data.room);
 	});
-	io.on("new-player-joined", function(data){
+	socket.on("new-player-joined", function(data){
 		$("body").append("Player joined: "+data.playerId);
 	});
 
