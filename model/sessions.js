4409b2c8ff31a21035fd6e097b86a977adafab2f
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-03-28 21:20:55 +0100

    moves sessions to rooms

--- model/sessions.js
+++ model/sessions.js
@@ -1,8 +1,26 @@
-var crypto = require('crypto');
+var crypto = require('crypto'),
+	io = require('socket.io');
+
+var games = {};
 
 var newHash = function () {
 	var hash = crypto.randomBytes(20).toString('hex');
 	return hash;
-}
+};
 
 exports.newHash = newHash;
+
+var registerGame = function() {
+	var hash = newHash();
+
+	var gameState = {};
+	var gameSockets = io.of('/' + hash).on('connection', function(socket) {
+			socket.on('turnChanged', function(data) {
+
+			});
+		});
+
+	return hash;
+}
+
+
