a9ea1c65c681572935653fb449e88b468dc2f559
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-04-15 14:58:34 +0200

    these changes fixes some of the world's most stupid code

--- model/player-queue.js
+++ model/player-queue.js
@@ -1,27 +1,27 @@
 
 var addPlayer = function(player) {
 	this.players.push(player);
-}
+};
 
 var getCurrentPlayer = function() {
-	return this.players[this.currentPlayer]
-}
+	return this.players[this.currentPlayer];
+};
 
 var changeTurn = function() {
-	if(((this.getCurrentPlayer+1) % this.players.length) === 0)
+	this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
+	if (this.currentPlayer === 0)
 		this.currentTurn++;
-	this.currentPlayer = (this.currentPlayer + 1) % this.players.length
-}
+};
 
 var setCurrentPlayer = function (player) {
-	this.currentPlayer = this.players.indexOf(player)
-}
+	this.currentPlayer = this.players.indexOf(player);
+};
 
 exports.Queue = function () {
-	this.players = []
-	this.currentPlayer = 0
+	this.players = [];
+	this.currentPlayer = 0;
 	this.currentTurn = 1;
-}
+};
 
 exports.Queue.prototype = {
 	constructor: exports.Queue,
@@ -30,4 +30,4 @@ exports.Queue.prototype = {
 	getCurrentPlayer: getCurrentPlayer,
 	changeTurn: changeTurn,
 	setCurrentPlayer: setCurrentPlayer
-}
+};
