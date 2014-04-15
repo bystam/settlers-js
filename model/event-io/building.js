b474f3c84e985cb947f13f6545d9b0e079699a9a
Fredrik Bystam <fredrik.bystam@gmail.com>
2014-04-15 17:01:32 +0200

    changes double quotes to single quotes lol

--- model/event-io/building.js
+++ model/event-io/building.js
@@ -15,31 +15,33 @@ exports.setupBuildingEvents = function(socket, room, playerId){
 	var game = games[room];
 	var rules = game.rules;
 
-	socket.on("build-road", function(coords) {
+	socket.on('build-road', function(coords) {
 		var buildIsLegal = rules.roadBuildIsLegal(coords, playerId);
 		if (game.queue.currentTurn === 1)
 			buildIsLegal = rules.initialRoadBuildIsLegal(coords, playerId);
 		if (buildIsLegal)
 			placeRoad (coords, playerId, game);
-		io.sockets.in(room).emit("build-road", {playerId:playerId, coords:coords,
+		io.sockets.in(room).emit('build-road', {playerId:playerId, coords:coords,
 															allowed: buildIsLegal});
 	});
 
-	socket.on("build-settlement", function(coords){
+	socket.on('build-settlement', function(coords){
 		var buildIsLegal = rules.settlementBuildIsLegal(coords, playerId);
 		if (game.queue.currentTurn === 1)
 			buildIsLegal = rules.initialSettlementBuildIsLegal(coords, playerId);
 		if(buildIsLegal)
 			placeBuilding(coords, playerId, game);
-		io.sockets.in(room).emit("build-settlement", {playerId:playerId, coords: coords,
+		io.sockets.in(room).emit('build-settlement', {playerId:playerId, coords: coords,
 																									allowed: buildIsLegal});
 	});
 
-	socket.on("build-city", function(coords){
+	socket.on('build-city', function(coords){
 		var buildIsLegal = rules.cityBuildIsLegal(coords, playerId);
+		if (game.queue.currentTurn === 1)
+			buildIsLegal = false; // TODO let it be separate rule chain?
 		if(buildIsLegal)
 			placeCity(coords, playerId, game);
-		io.sockets.in(room).emit("build-city", {playerId:playerId, coords: coords,
+		io.sockets.in(room).emit('build-city', {playerId:playerId, coords: coords,
 																						allowed: buildIsLegal});
 	});
 };
